2b563d4b6d8ab255e99eb0ad9de9a8f4c793be10
tombartindale <tom@bartindale.com>
2017-11-27 15:06:44 +0000

    export

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ ISTHERE=$(curl -s --head -w %{http_code} https://hub.docker.com/v2/repositories/
 
 if [ $ISTHERE -ne '200' ]
 then
-    export VERSION=PACKAGE_VERSION
+    export VERSION=$PACKAGE_VERSION
     echo "VERSION DOES NOT EXIST, TAGGING"
 else
     echo "INCREMENT VERSION USING npm version BEFORE RE-RUNNING"
