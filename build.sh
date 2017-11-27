fae1a2776a8368fcd02d3f0a03edd25aa490738a
tombartindale <tom@bartindale.com>
2017-11-27 14:30:55 +0000

    updated CI

---
+++ build.sh
@@ -0,0 +1,16 @@
+# GET VERSION:
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+echo "BUILDING FOR VERSION $PACKAGE_VERSION"
+
+# pull latest version
+ISTHERE=$(curl -s --head -w %{http_code} https://hub.docker.com/v2/repositories/connectedacademy/gossipmill/tags/$PACKAGE_VERSION/ -o /dev/null)
+
+if [ $ISTHERE -ne '200' ]
+then
+    echo "VERSION DOES NOT EXIST, TAGGING"
+else
+    echo "INCREMENT VERSION USING npm version BEFORE RE-RUNNING"
+    exit 1
+fi
